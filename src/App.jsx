0d55b8efaad9297908a6453acf7e16d64d02eaab
Victor <vonvicky123@gmail.com>
2026-02-15 22:33:44 +0300

    Initial commit of 3D journal project with High-End Lighting

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Canvas } from '@react-three/fiber'
+import { useGLTF, PresentationControls, Environment, ContactShadows, Center } from '@react-three/drei'
+import './App.css'
+
+function Model(props) {
+  const { scene } = useGLTF('/lamborghini.glb')
+  return <primitive object={scene} {...props} />
+}
+
+export default function App() {
+  return (
+    <Canvas dpr={[1, 2]} shadows camera={{ fov: 25 }} style={{ position: "absolute" }}>
+      <color attach="background" args={['#101010']} />
+
+      {/* High-End Environment & Lighting */}
+      <Environment preset="city" />
+      <ambientLight intensity={0.5} />
+
+      <PresentationControls speed={1.5} global zoom={0.5} polar={[-0.1, Math.PI / 4]}>
+        <Center>
+          <Model scale={0.9} />
+        </Center>
+      </PresentationControls>
+
+      {/* Realistic Shadow */}
+      <ContactShadows position={[0, -1.5, 0]} opacity={0.4} scale={10} blur={2.5} far={4} />
+    </Canvas>
+  )
+}
