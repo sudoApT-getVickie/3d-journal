edc79f5950f6e0251c73c33d0e1da08c9a91a162
Victor <vonvicky123@gmail.com>
2026-02-16 21:03:30 +0300

    Update scene elements and HTML config

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { useGLTF, PresentationControls, Environment, ContactShadows, Center, ScrollControls, useScroll, Float, Sparkles } from '@react-three/drei'
+import { useGLTF, PresentationControls, Environment, ContactShadows, Center, ScrollControls, useScroll, Float, Sparkles, Scroll } from '@react-three/drei'
 import * as THREE from 'three'
 import './App.css'
 
@@ -53,28 +53,76 @@ function LamborghiniScene() {
 export default function App() {
   return (
     <Canvas dpr={[1, 2]} shadows camera={{ fov: 45 }} style={{ position: "absolute" }}>
-      {/* Initial background color will be managed by useFrame */}
+      {/* Initial background color (will be updated by the lerp in LamborghiniScene) */}
       <color attach="background" args={['#050506']} />
 
-      {/* High-End Environment & Lighting */}
+      {/* Lighting & Env */}
       <Environment preset="city" />
       <ambientLight intensity={0.5} />
 
-      {/* Particles: Added gold color to match */}
+      {/* Particles */}
       <Sparkles
         count={50}
-        scale={10}
-        size={4}
+        scale={8}
+        size={5}
         speed={0.4}
-        opacity={0.5}
+        opacity={0.7}
         color="#E5D352"
       />
 
+      {/* SCROLL CONTROLS WRAPPER */}
       <ScrollControls pages={3} damping={0.2}>
+
+        {/* Layer 1: The 3D Scene (Your existing component) */}
         <LamborghiniScene />
+
+        {/* Layer 2: The HTML Text Overlay (NEW ADDITION) */}
+        <Scroll html style={{ width: '100%' }}>
+
+          {/* SECTION 1: TITLE */}
+          <section style={{ height: '100vh', display: 'flex', alignItems: 'center', paddingLeft: '5vw' }}>
+            <h1 style={{
+              fontFamily: '"Oswald", sans-serif',
+              fontSize: '12vw',
+              fontWeight: '800',
+              color: '#E5D352',
+              lineHeight: '0.8',
+              margin: 0
+            }}>
+              LAMBO<br />RGHINI
+            </h1>
+          </section>
+
+          {/* SECTION 2: DESCRIPTION (Right Aligned) */}
+          <section style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'flex-end', paddingRight: '5vw' }}>
+            <div style={{ textAlign: 'right' }}>
+              <h2 style={{ fontFamily: '"Oswald", sans-serif', fontSize: '4vw', color: '#fff', margin: 0 }}>
+                DEFINE
+              </h2>
+              <h2 style={{ fontFamily: '"Oswald", sans-serif', fontSize: '6vw', color: '#E5D352', margin: 0 }}>
+                LEGEND
+              </h2>
+            </div>
+          </section>
+
+          {/* SECTION 3: FOOTER (Center) */}
+          <section style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <h1 style={{
+              fontFamily: '"Oswald", sans-serif',
+              fontSize: '15vw',
+              color: 'transparent',
+              WebkitTextStroke: '1px #E5D352', // Outline effect
+              opacity: 0.8,
+              margin: 0
+            }}>
+              FUTURE
+            </h1>
+          </section>
+
+        </Scroll>
       </ScrollControls>
 
-      {/* Realistic Shadow */}
+      {/* Shadow */}
       <ContactShadows position={[0, -1.5, 0]} opacity={0.4} scale={10} blur={2.5} far={4} />
     </Canvas>
   )
